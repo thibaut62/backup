16d9d27a0bb1078df912cc8d890f350bb54177e6
thibaut62 <delville.thibaut+git@gmail.com>
2022-01-30 16:56:29 +0100

    modif pc fix arras

--- menu.sh
+++ menu.sh
@@ -85,66 +85,59 @@ DEST_HDD=$($DIALOG --title "Emplacement du Disque de sauvegarde" --dselect / 0 0
 # Debut du menu #
 #################
 
-#DIALOG=${DIALOG=dialog}
+menu()
+  {
+  HEIGHT=0
+  WIDTH=0
+  CHOICE_HEIGHT=0
+  BACKTITLE=""
+  TITLE="Rsync et Backup"
+  MENU="Choissisez parmi les options suivantes:"
+  
+  OPTIONS=(1 "Rsync PC-FIXE Manjaro"
+           2 "Backup PC-FIXE Manjaro"
+           3 "Rsync PC Fixe Arras"
+           4 "Backup PC Fixe Arras"
+           5 "Info Disque de sauvegarde"
+           6 "Quit")
+  
+  CHOICE=$($DIALOG --clear \
+                  --backtitle "$BACKTITLE" \
+                  --title "$TITLE" \
+                  --menu "$MENU" \
+                  $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                  "${OPTIONS[@]}" \
+                  2>&1 >/dev/tty)
+  
+  clear
+  case $CHOICE in
+          1)
+              synchro "$SOURCE_RSYNC_PCFIXE_MANJARO" "$DEST_HDD$DEST_RSYNC_PCFIXE_MANJARO" "$EXCLUDE_RSYNC_PCFIXE_MANJARO"
+              menu
+              ;;
+          2)
+              backup "$SOURCE_BACKUP_PCFIXE_MANJARO" "$DEST_HDD$DEST_BACKUP_PCFIXE_MANJARO" "$EXCLUDE_BACKUP_PCFIXE_MANJARO"
+              menu
+              ;;
+          3)
+              synchro "$SOURCE_RSYNC_MEDION_MANJARO" "$DEST_HDD$DEST_RSYNC_ARRAS_WINDOWS" "$EXCLUDE_RSYNC_PCFIXE_ARRAS"
+              menu
+              ;;
+          4)
+              backup "$SOURCE_BACKUP_PCFIXE_ARRAS" "$DEST_HDD$DEST_BACKUP_PCFIXE_ARRAS" "$EXCLUDE_BACKUP_PCFIXE_ARRAS"
+              menu
+              ;;
+          5)
+              UTILISE=$(df -h | grep thibaut/backup | awk '{print $3}')
+              LIBRE=$(  df -h | grep thibaut/backup | awk '{print $4}')
+              TAILLE=$( df -h | grep thibaut/backup | awk '{print $2}')
+              $DIALOG --msgbox "Il reste $LIBRE/$TAILLE sur le support de stockage utilisé pour la sauvegarde." 0 0
+              menu
+              ;;
+          6)
+              exit
+              ;;            
+  esac
+}  
 
-HEIGHT=0
-WIDTH=0
-CHOICE_HEIGHT=0
-BACKTITLE=""
-TITLE="Rsync et Backup"
-MENU="Choissisez parmi les options suivantes:"
-
-OPTIONS=(1 "Rsync PC-FIXE Manjaro"
-         2 "Backup PC-FIXE Manjaro"
-         3 "Rsync Medion Manjaro"
-         4 "Rsync Medion Windows"
-         5 "Backup Medion Manjaro"
-         6 "Backup Medion Windows"
-         7 "Info Disque de sauvegarde"
-         8 "Quit")
-
-CHOICE=$($DIALOG --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-case $CHOICE in
-        1)
-            synchro "$SOURCE_RSYNC_PCFIXE_MANJARO" "$DEST_HDD$DEST_RSYNC_PCFIXE_MANJARO" "$EXCLUDE_RSYNC_PCFIXE_MANJARO"
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        2)
-            backup "$SOURCE_BACKUP_PCFIXE_MANJARO" "$DEST_HDD$DEST_BACKUP_PCFIXE_MANJARO" "$EXCLUDE_BACKUP_PCFIXE_MANJARO"
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        3)
-            synchro "$SOURCE_RSYNC_MEDION_MANJARO" "$DEST_HDD$DEST_RSYNC_MEDION_MANJARO" "$EXCLUDE_RSYNC_MEDION_MANJARO"
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        4)
-            synchro "$SOURCE_RSYNC_MEDION_WINDOWS" "$DEST_HDD$DEST_RSYNC_MEDION_WINDOWS" "$EXCLUDE_RSYNC_MEDION_WINDOWS"
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        5)
-            backup "$SOURCE_BACKUP_MEDION_MANJARO" "$DEST_HDD$DEST_BACKUP_MEDION_MANJARO" "$EXCLUDE_BACKUP_MEDION_MANJARO"
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        6)
-            backup "$SOURCE_BACKUP_MEDION_WINDOWS" "$DEST_HDD$DEST_BACKUP_MEDION_WINDOWS" "$EXCLUDE_BACKUP_MEDION_WINDOWS"
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        7)
-            UTILISE=$(df -h | grep thibaut/backup | awk '{print $3}')
-            LIBRE=$(  df -h | grep thibaut/backup | awk '{print $4}')
-            TAILLE=$( df -h | grep thibaut/backup | awk '{print $2}')
-            $DIALOG --msgbox "Il reste $LIBRE/$TAILLE sur le support de stockage utilisé pour la sauvegarde." 0 0
-            bash $CHEMIN/scripts/backup/menu.sh
-            ;;
-        8)
-            exit
-            ;;            
-esac
+menu
